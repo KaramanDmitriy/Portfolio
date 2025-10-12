37765b8ab9d54ddb99ab7dfdf359cc17e8749d02
KaramanDmitriy <karamandmitriy@gmail.com>
2025-10-13 00:30:56 +0300

    fix margin portfolio

--- src/assets/pages/Portfolio.jsx
+++ src/assets/pages/Portfolio.jsx
@@ -3,10 +3,10 @@ import { Suspense, lazy } from 'react';
 export default function Portfolio() {
     const PortfolioBlock = lazy(() => import('../components/PortfolioBlock'));
     return (
-        <>
+        <div className="portfolio-page">
             <Suspense fallback={<div>Завантаження...</div>}>
                 <PortfolioBlock />
             </Suspense>
-        </>
+        </div>
     )
 }
